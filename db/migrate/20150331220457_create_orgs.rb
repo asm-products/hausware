3a4f1925e96eebb62ebc4b1dce756a86e7a48f5d
Matthew Moore <m3moore@gmail.com>
2015-04-13 16:47:50 -0700

    Attempt to fix migration issue with Orgs

--- db/migrate/20150331220457_create_orgs.rb
+++ db/migrate/20150331220457_create_orgs.rb
@@ -5,9 +5,10 @@ class CreateOrgs < ActiveRecord::Migration
       t.string :permalink, index: true
       t.string :email
       t.string :phone
-      t.references :owner, references: :users, index: true, foreign_key: true
+      t.references :owner, references: :users, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :orgs, :users, column: :owner_id
   end
 end
